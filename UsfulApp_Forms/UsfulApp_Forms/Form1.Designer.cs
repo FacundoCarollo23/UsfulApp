926552bba8bb2a111630d977ba125b1c628252dc
FacundoCarollo <facukiller129@gmail.com>
2021-07-14 15:40:59 -0300

    Menú parte 1

--- UsfulApp_Forms/UsfulApp_Forms/Form1.Designer.cs
+++ UsfulApp_Forms/UsfulApp_Forms/Form1.Designer.cs
@@ -29,45 +29,207 @@ namespace UsfulApp_Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Nav = new System.Windows.Forms.Panel();
+            this.iconButton3 = new FontAwesome.Sharp.IconButton();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.Calendar = new FontAwesome.Sharp.IconButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Logo = new System.Windows.Forms.PictureBox();
             this.header = new System.Windows.Forms.Panel();
+            this.Home2 = new System.Windows.Forms.Label();
+            this.Home = new FontAwesome.Sharp.IconPictureBox();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.Nav.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Home)).BeginInit();
             this.SuspendLayout();
             // 
             // Nav
             // 
+            this.Nav.BackColor = System.Drawing.Color.Navy;
+            this.Nav.Controls.Add(this.iconButton3);
+            this.Nav.Controls.Add(this.iconButton2);
+            this.Nav.Controls.Add(this.iconButton1);
+            this.Nav.Controls.Add(this.Calendar);
+            this.Nav.Controls.Add(this.panel1);
             this.Nav.Dock = System.Windows.Forms.DockStyle.Left;
             this.Nav.Location = new System.Drawing.Point(0, 0);
             this.Nav.Name = "Nav";
-            this.Nav.Size = new System.Drawing.Size(157, 799);
+            this.Nav.Size = new System.Drawing.Size(194, 829);
             this.Nav.TabIndex = 0;
+            // 
+            // iconButton3
+            // 
+            this.iconButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton3.FlatAppearance.BorderSize = 0;
+            this.iconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconButton3.IconChar = FontAwesome.Sharp.IconChar.CalendarWeek;
+            this.iconButton3.IconColor = System.Drawing.SystemColors.MenuBar;
+            this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton3.IconSize = 32;
+            this.iconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton3.Location = new System.Drawing.Point(0, 332);
+            this.iconButton3.Name = "iconButton3";
+            this.iconButton3.Padding = new System.Windows.Forms.Padding(20, 0, 10, 0);
+            this.iconButton3.Size = new System.Drawing.Size(194, 44);
+            this.iconButton3.TabIndex = 7;
+            this.iconButton3.Text = "iconButton3";
+            this.iconButton3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton3.UseVisualStyleBackColor = true;
+            this.iconButton3.Click += new System.EventHandler(this.iconButton3_Click);
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton2.FlatAppearance.BorderSize = 0;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.CalendarWeek;
+            this.iconButton2.IconColor = System.Drawing.SystemColors.MenuBar;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.IconSize = 32;
+            this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton2.Location = new System.Drawing.Point(0, 282);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Padding = new System.Windows.Forms.Padding(20, 0, 10, 0);
+            this.iconButton2.Size = new System.Drawing.Size(194, 44);
+            this.iconButton2.TabIndex = 6;
+            this.iconButton2.Text = "iconButton2";
+            this.iconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.CalendarWeek;
+            this.iconButton1.IconColor = System.Drawing.SystemColors.MenuBar;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 32;
+            this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.Location = new System.Drawing.Point(0, 228);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Padding = new System.Windows.Forms.Padding(20, 0, 10, 0);
+            this.iconButton1.Size = new System.Drawing.Size(194, 44);
+            this.iconButton1.TabIndex = 5;
+            this.iconButton1.Text = "iconButton1";
+            this.iconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // Calendar
+            // 
+            this.Calendar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Calendar.FlatAppearance.BorderSize = 0;
+            this.Calendar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Calendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calendar.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Calendar.IconChar = FontAwesome.Sharp.IconChar.CalendarWeek;
+            this.Calendar.IconColor = System.Drawing.SystemColors.MenuBar;
+            this.Calendar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Calendar.IconSize = 32;
+            this.Calendar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Calendar.Location = new System.Drawing.Point(0, 178);
+            this.Calendar.Name = "Calendar";
+            this.Calendar.Padding = new System.Windows.Forms.Padding(20, 0, 10, 0);
+            this.Calendar.Size = new System.Drawing.Size(194, 44);
+            this.Calendar.TabIndex = 4;
+            this.Calendar.Text = "Calendar";
+            this.Calendar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Calendar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Calendar.UseVisualStyleBackColor = true;
+            this.Calendar.Click += new System.EventHandler(this.Calendar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Logo);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 175);
+            this.panel1.TabIndex = 0;
+            // 
+            // Logo
+            // 
+            this.Logo.Image = ((System.Drawing.Image)(resources.GetObject("Logo.Image")));
+            this.Logo.Location = new System.Drawing.Point(22, 17);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(149, 136);
+            this.Logo.TabIndex = 0;
+            this.Logo.TabStop = false;
+            this.Logo.Click += new System.EventHandler(this.Logo_Click);
             // 
             // header
             // 
+            this.header.BackColor = System.Drawing.Color.Navy;
+            this.header.Controls.Add(this.Home2);
+            this.header.Controls.Add(this.Home);
             this.header.Dock = System.Windows.Forms.DockStyle.Top;
-            this.header.Location = new System.Drawing.Point(157, 0);
+            this.header.Location = new System.Drawing.Point(194, 0);
             this.header.Name = "header";
-            this.header.Size = new System.Drawing.Size(1315, 45);
+            this.header.Size = new System.Drawing.Size(1278, 71);
             this.header.TabIndex = 1;
+            // 
+            // Home2
+            // 
+            this.Home2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Home2.Location = new System.Drawing.Point(82, 24);
+            this.Home2.Name = "Home2";
+            this.Home2.Padding = new System.Windows.Forms.Padding(0, 2, 0, 0);
+            this.Home2.Size = new System.Drawing.Size(45, 20);
+            this.Home2.TabIndex = 1;
+            this.Home2.Text = "Home";
+            // 
+            // Home
+            // 
+            this.Home.BackColor = System.Drawing.Color.Navy;
+            this.Home.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.Home.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.Home.IconColor = System.Drawing.Color.MediumSlateBlue;
+            this.Home.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Home.Location = new System.Drawing.Point(44, 17);
+            this.Home.Name = "Home";
+            this.Home.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.Home.Size = new System.Drawing.Size(32, 32);
+            this.Home.TabIndex = 0;
+            this.Home.TabStop = false;
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(157, 67);
+            this.panel3.BackColor = System.Drawing.Color.DarkBlue;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(194, 71);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1314, 732);
+            this.panel3.Size = new System.Drawing.Size(1278, 758);
             this.panel3.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1472, 799);
+            this.ClientSize = new System.Drawing.Size(1472, 829);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.header);
             this.Controls.Add(this.Nav);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Nav.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.header.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Home)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -77,6 +239,14 @@ namespace UsfulApp_Forms
         private System.Windows.Forms.Panel Nav;
         private System.Windows.Forms.Panel header;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconButton Calendar;
+        private System.Windows.Forms.PictureBox Logo;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private System.Windows.Forms.Label Home2;
+        private FontAwesome.Sharp.IconPictureBox Home;
     }
 }
 
--- UsfulApp_Forms/UsfulApp_Forms/Form1.cs
+++ UsfulApp_Forms/UsfulApp_Forms/Form1.cs
@@ -8,22 +8,117 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using FontAwesome.Sharp;
+using System.Runtime.InteropServices;
 
 namespace UsfulApp_Forms
 {
     public partial class Form1 : Form
     {
+        #region Menú y conección BD
+        private IconButton currentBtn;
+        private Panel leftBorder;
+        private Form currentChildForm;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conexion c = new Conexion();
+            leftBorder = new Panel();
+            leftBorder.Size = new Size(8, 44);
+            Nav.Controls.Add(leftBorder);
+
+        }
+
+        private struct RgbColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(60, 82, 45);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(64, 224, 208);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(255, 255, 0);
+            public static Color color7 = Color.FromArgb(238, 107, 0);
+            public static Color color8 = Color.FromArgb(0, 104, 0);
+        }
+
+        private void ActivateButtom(object senderBtn, Color color)
+        {
+            CalendarButtom();
+            if (senderBtn != null)
+            {
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(0, 0, 128);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleCenter;
+
+                leftBorder.BackColor = color;
+                leftBorder.Location = new Point(0, currentBtn.Location.Y);
+                leftBorder.Visible = true;
+                leftBorder.BringToFront();
+                Home.IconChar = currentBtn.IconChar;
+                Home.IconColor = color;
+            }
+        }
+
+        private void CalendarButtom()
+        {
+            if (currentBtn != null)
+            {
+
+                currentBtn.BackColor = Color.FromArgb(0,0,128) ;
+                currentBtn.ForeColor = Color.Gainsboro;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.Gainsboro;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+                Home.IconChar = IconChar.Home;
+                Home.IconColor = Color.Gainsboro;
+                Home2.Text = "Home";
+            }
+        }
+
+        private void Calendar_Click(object sender, EventArgs e)
+        {
+            ActivateButtom(sender, RgbColors.color3);
+        }
+
+        private void Reset()
+        {
+            CalendarButtom();
+            leftBorder.Visible = false;
+        }
+
+        private void Logo_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
 
-            Conexion c = new Conexion();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
           
         }
+
+      
+
+        private void iconPictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void iconButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
     
 }
